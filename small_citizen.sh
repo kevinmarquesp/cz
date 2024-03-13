1d7e0607de32c5dfabcf3fab4f37a45594b0694e
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-03-13 15:02:14 -0300

    doc (comment): added a description comment for that section

--- small_citizen.sh
+++ small_citizen.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+## ARGUMENT PARSER
+## 	For this application, the arguments should'b be so flexible, because it has
+##  a specific propurse. No need to put too much arguments
+
 OPTIONS="h,a,d,j:"
 LONG_OPTIONS="help,ammend,description,json:"
 
 eval -- set -- $(getopt --name "${BASH_SOURCE}" --options "${OPTIONS}" \
 						--longoptions "${LONG_OPTIONS}" -- "${@}")
-
 unset OPTIONS LONG_OPTIONS
 
