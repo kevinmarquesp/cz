930e0fd9e0fcd187af18dd90fee2068c309daa69
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-03-13 15:03:34 -0300

    wip: parsing the --help option

--- small_citizen.sh
+++ small_citizen.sh
@@ -11,3 +11,26 @@ eval -- set -- $(getopt --name "${BASH_SOURCE}" --options "${OPTIONS}" \
 						--longoptions "${LONG_OPTIONS}" -- "${@}")
 unset OPTIONS LONG_OPTIONS
 
+## OPTIONS SETUP
+## 	This section will asing/overwrite values to the options variables, most of
+##  is bolean variables that the script will use to know how to build the git
+##  commit command
+
+while true
+do
+	case "$1" in
+		#todo: write a good help command
+
+		"-h" | "--help")
+			echo
+			echo "${BASH_SOURCE} - v0.2.0"
+			echo
+			echo ""
+			exit
+		;;
+
+		"--")
+			break
+		;;
+	esac
+done
